b9874a729d7e841f8ef9456616f012842136e792
Sabrina Frazão <sabrinadasilvafrazao@gmail.com>
2023-05-28 17:02:20 -0400

    mini projeto finalizado, objetivo: incrementa futuramente

--- projeto_cad_usuarios/app_cad_usuarios/views.py
+++ projeto_cad_usuarios/app_cad_usuarios/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
+from .models import Usuario
 
 
 def home(request):
     return render(request, 'usuarios/home.html')
+
+
+def usuarios(request):
+    # Salvar os dados da tela para o banco de dados
+    novo_usuario = Usuario()
+    novo_usuario.nome = request.POST.get('nome')
+    novo_usuario.idade = request.POST.get('idade')
+    novo_usuario.save()
+
+    # Exibir todos os usuários já cadastrados em uma nova página
+    usuarios = {
+        'usuarios': Usuario.objects.all()
+    }
+
+    # Retornar os dados para página de listagem de usários
+    return render(request, 'usuarios/usuarios.html', usuarios)
